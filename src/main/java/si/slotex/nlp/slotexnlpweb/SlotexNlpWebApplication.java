cd73b8e90865b58138c07616a5faf0c70ff8a568
JakobBernik <48559160+JakobBernik@users.noreply.github.com>
2019-04-01 21:02:10 +0200

    initial Maven project (#4)
    * initial Maven project with correct pom formatting

---
+++ src/main/java/si/slotex/nlp/slotexnlpweb/SlotexNlpWebApplication.java
@@ -0,0 +1,13 @@
+package si.slotex.nlp.slotexnlpweb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SlotexNlpWebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SlotexNlpWebApplication.class, args);
+	}
+
+}
